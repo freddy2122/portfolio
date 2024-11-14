08bf77840fd9e777caa6cc4acf04d1ab7560d1b1
freddy2122 <99898817+freddy2122@users.noreply.github.com>
2024-11-14 16:23:27 +0100

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,7 +10,7 @@ const Contact = () => {
     <>
       <Header />
 
-      <div className="container pt-5">
+      <div className="container pt-5 mt-5">
         <div className="row  g-lg-5 py-5">
           <div className="col-md-1 "></div>
           <div className="col-md-5 text-center text-lg-start">
